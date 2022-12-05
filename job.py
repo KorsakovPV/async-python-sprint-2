cdc61cddc6df867e31c45fad7ef0d87ddd4edd2f
Pavel Korsakov <62927073+KorsakovPV@users.noreply.github.com>
2022-12-06 02:18:22 +0700

    Initial commit

---
+++ job.py
@@ -0,0 +1,12 @@
+class Job:
+    def __init__(self, start_at="", max_working_time=-1, tries=0, dependencies=[]):
+        pass
+
+    def run(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def stop(self):
+        pass
---
+++ scheduler.py
@@ -0,0 +1,15 @@
+class Scheduler:
+    def __init__(self, pool_size=10):
+        pass
+
+    def schedule(self, task):
+        pass
+
+    def run(self):
+        pass
+
+    def restart(self):
+        pass
+
+    def stop(self):
+        pass
